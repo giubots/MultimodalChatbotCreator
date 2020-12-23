582522cdff3692b229b496d3aa1fe34e959a75eb
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2020-12-23 23:30:50 +0100

    added test rest api backup server

---
+++ backend/rest_server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import json
+
+# import framework
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'framework'))
+from framework import Framework
+from parameters import *
+
+app = Flask(__name__)
+api = Api(app)
+
+
+# create REST backup API
+class Event(Resource):
+    def get(self):
+        return {'response': 'ok'}, 200
+
+
+api.add_resource(Event, '/event')
+
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app for the REST API on port 5000
