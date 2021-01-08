c578fdb20f21607871858764400ae301ccf0e8e0
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2021-01-08 01:52:58 +0100

    moved files around

---
+++ backend/examples/quickstart/example_clients/websocket_client.py
@@ -0,0 +1,63 @@
+import asyncio
+import websockets
+import json
+from functions import *
+
+
+# async def hello():
+#     uri = "ws://localhost:8765"
+#     async with websockets.connect(uri) as websocket:
+#         name = input("What's your name? ")
+#
+#         await websocket.send(name)
+#         print(f"> {name}")
+#
+#         greeting = await websocket.recv()
+#         print(f"< {greeting}")
+#
+#
+# asyncio.get_event_loop().run_until_complete(hello())
+
+async def client():
+    uri = "ws://localhost:8765"
+    params = {
+        # interaction is None when you are starting it,
+        # then you need to set it to the initial websocket key value
+        # to continue the interaction.
+        'interaction': None,
+        # the user id identifies the user in the backend and is required
+        'uid': 'user id'
+    }
+    async with websockets.connect(add_params(uri, params)) as websocket:
+        # this is the websocket key value you need to provide
+        # if you want to start a new websocket connection for the
+        # same interaction.
+        # if the interaction is set to None, a new interaction will be
+        # created. (and a new Framework process instantiated)
+        # see websocket_client_interrupted.py for more information
+        if params['interaction'] is None:
+            ws_key = await websocket.recv()
+
+        event_utterance = {
+            'type': 'utterance',
+            'utterance': 'my_utterance'  # this is an example text
+        }
+
+        event_data = {
+            'type': 'data',
+            'payload': {
+                'echo': 'Bob'  # this is an example data
+            }
+        }
+
+        # switch between test utterance or data
+        event = event_data
+
+        await websocket.send(json.dumps(event))
+        print(f"> {event}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+
+asyncio.get_event_loop().run_until_complete(client())
