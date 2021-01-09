e2f58407dccfd9169edc71eecc2aa3464c8369d1
giubots <giulioantonio.abbo@gmail.com>
2021-01-09 22:05:37 +0100

    Added tests for framework correct initialization

--- framework/tests/test_framework.py
+++ framework/tests/test_framework.py
@@ -1,6 +1,88 @@
 from unittest import TestCase
 
 from mccreator_framework.framework import *
+from mccreator_framework.nlu_adapters import NoNluAdapter
+
+
+class TestFramework(TestCase):
+    @staticmethod
+    def callback_getter(name):
+        return lambda d, k, c: Response(k, c, True)
+
+    def setUp(self) -> None:
+        self.my_kb = {"my_key": "a value"}
+        self.my_ctx = {"ctx_key": "ctx value"}
+        self.my_callback = self.callback_getter
+        self.my_nlu = NoNluAdapter([])
+        self.my_save = lambda k: None
+
+    def test_init(self):
+        my_proc = Process([Activity("one", None, ActivityType.START)], "one")
+
+        my_framework = Framework(my_proc, self.my_kb, self.my_ctx, self.my_callback, self.my_nlu, self.my_save)
+        self.assertEqual(my_framework._process, my_proc)
+        self.assertEqual(my_framework._kb, self.my_kb)
+        self.assertEqual(my_framework._ctx, self.my_ctx)
+        self.assertEqual(my_framework._callback_getter, self.my_callback)
+        self.assertEqual(my_framework._nlu, self.my_nlu)
+        self.assertEqual(my_framework._on_save, self.my_save)
+
+    def test_init_dict(self):
+        proc_dic = {
+            "first_activity_id": "one",
+            "activities": [{"my_id": "one", "next_id": None, "my_type": "task"}]
+        }
+        my_framework = Framework(proc_dic, self.my_kb, self.my_ctx, self.my_callback, self.my_nlu, self.my_save)
+        self.assertEqual(my_framework._process.first.id, proc_dic["first_activity_id"])
+        self.assertEqual(my_framework._process.activities[0].id, proc_dic["activities"][0]["my_id"])
+        self.assertEqual(my_framework._process.activities[0].next_id, proc_dic["activities"][0]["next_id"])
+        self.assertEqual(my_framework._process.activities[0].type.value, proc_dic["activities"][0]["my_type"])
+        self.assertEqual(my_framework._kb, self.my_kb)
+        self.assertEqual(my_framework._ctx, self.my_ctx)
+        self.assertEqual(my_framework._callback_getter, self.my_callback)
+        self.assertEqual(my_framework._nlu, self.my_nlu)
+        self.assertEqual(my_framework._on_save, self.my_save)
+
+    def test_init_callback(self):
+        my_proc = Process([Activity("one", None, ActivityType.START)], "one")
+
+        def proc_call():
+            return my_proc
+
+        def kb_call():
+            return self.my_kb
+
+        my_framework = Framework(proc_call, kb_call, self.my_ctx, self.my_callback, self.my_nlu, self.my_save)
+
+        self.assertEqual(my_framework._process, my_proc)
+        self.assertEqual(my_framework._kb, self.my_kb)
+        self.assertEqual(my_framework._ctx, self.my_ctx)
+        self.assertEqual(my_framework._callback_getter, self.my_callback)
+        self.assertEqual(my_framework._nlu, self.my_nlu)
+        self.assertEqual(my_framework._on_save, self.my_save)
+
+    def test_init_callback_dict(self):
+        proc_dic = {
+            "first_activity_id": "one",
+            "activities": [{"my_id": "one", "next_id": None, "my_type": "task"}]
+        }
+
+        def proc_call():
+            return proc_dic
+
+        def kb_call():
+            return self.my_kb
+
+        my_framework = Framework(proc_call, kb_call, self.my_ctx, self.my_callback, self.my_nlu, self.my_save)
+        self.assertEqual(my_framework._process.first.id, proc_dic["first_activity_id"])
+        self.assertEqual(my_framework._process.activities[0].id, proc_dic["activities"][0]["my_id"])
+        self.assertEqual(my_framework._process.activities[0].next_id, proc_dic["activities"][0]["next_id"])
+        self.assertEqual(my_framework._process.activities[0].type.value, proc_dic["activities"][0]["my_type"])
+        self.assertEqual(my_framework._kb, self.my_kb)
+        self.assertEqual(my_framework._ctx, self.my_ctx)
+        self.assertEqual(my_framework._callback_getter, self.my_callback)
+        self.assertEqual(my_framework._nlu, self.my_nlu)
+        self.assertEqual(my_framework._on_save, self.my_save)
 
 
 class TestResponse(TestCase):
