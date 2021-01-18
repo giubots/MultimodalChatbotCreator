c9896b6dad5c3a5ea0cbec53c27c114b0fbc9a9d
giubots <giulioantonio.abbo@gmail.com>
2021-01-18 20:42:40 +0100

    Added some test interactions

--- framework/tests/test_framework.py
+++ framework/tests/test_framework.py
@@ -4,9 +4,243 @@ from mmcc_framework.framework import *
 from mmcc_framework.nlu_adapters import NoNluAdapter
 
 
+class TestFrameworkBehaviourOR(TestCase):
+    @staticmethod
+    def callback_getter(_):
+        return lambda d, k, c: Response(k, c, True, choice=d["choice"] if "choice" in d else "")
+
+    def setUp(self) -> None:
+        self.my_framework = Framework(
+            Process(first_activity_id="start",
+                    activities=[Activity("start", "gateway", ActivityType.START),
+                                Activity("gateway", "end", ActivityType.OR, ["xor", "par", "or"]),
+                                Activity("xor", None, ActivityType.XOR, ["A", "B"]),
+                                Activity("A", None, ActivityType.TASK),
+                                Activity("B", None, ActivityType.TASK),
+                                Activity("par", None, ActivityType.PARALLEL, ["C", "D"]),
+                                Activity("C", None, ActivityType.TASK),
+                                Activity("D", None, ActivityType.TASK),
+                                Activity("or", None, ActivityType.OR, ["E", "F"]),
+                                Activity("E", None, ActivityType.TASK),
+                                Activity("F", None, ActivityType.END),
+                                Activity("end", None, ActivityType.END)]),
+            {"my_key": "a value"},
+            {"ctx_key": "ctx value"},
+            self.callback_getter,
+            NoNluAdapter([]),
+            lambda k: None)
+
+    def test_path_1(self):
+        path = [["", "gateway", []],
+                ["xor", "xor", ["gateway"]],
+                ["A", "A", ["gateway"]],
+                ["", "gateway", ["gateway"]],
+                ["par", "par", ["gateway"]],
+                ["C", "C", ["gateway"]],
+                ["", "par", ["gateway"]],
+                ["D", "D", ["gateway", "par"]],
+                ["", "par", ["gateway", "par"]],
+                [None, "gateway", ["gateway"]],
+                ["or", "or", ["gateway"]],
+                ["E", "E", ["gateway", "or"]],
+                ["", "or", ["gateway", "or"]],
+                [None, "gateway", ["gateway"]],
+                ["or", "or", ["gateway"]],
+                ["F", "F", ["gateway", "or"]],
+                ["", "F", ["gateway", "or"]]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+    def test_path_2(self):
+        path = [["", "gateway", []],
+                ["xor", "xor", ["gateway"]],
+                ["B", "B", ["gateway"]],
+                ["", "gateway", ["gateway"]],
+                ["or", "or", ["gateway"]],
+                ["E", "E", ["gateway", "or"]],
+                ["", "or", ["gateway", "or"]],
+                [None, "gateway", ["gateway"]],
+                ["par", "par", ["gateway"]],
+                ["D", "D", ["gateway"]],
+                ["", "par", ["gateway"]],
+                ["C", "C", ["gateway", "par"]],
+                ["", "par", ["gateway", "par"]],
+                ["D", "D", ["gateway", "par"]],
+                ["", "par", ["gateway", "par"]],
+                [None, "gateway", ["gateway"]],
+                [None, "end", []],
+                ["", "end", []]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+
+class TestFrameworkBehaviourPAR(TestCase):
+    @staticmethod
+    def callback_getter(_):
+        return lambda d, k, c: Response(k, c, True, choice=d["choice"] if "choice" in d else "")
+
+    def setUp(self) -> None:
+        self.my_framework = Framework(
+            Process(first_activity_id="start",
+                    activities=[Activity("start", "gateway", ActivityType.START),
+                                Activity("gateway", "end", ActivityType.PARALLEL, ["xor", "par", "or"]),
+                                Activity("xor", None, ActivityType.XOR, ["A", "B"]),
+                                Activity("A", None, ActivityType.TASK),
+                                Activity("B", None, ActivityType.TASK),
+                                Activity("par", None, ActivityType.PARALLEL, ["C", "D"]),
+                                Activity("C", None, ActivityType.TASK),
+                                Activity("D", None, ActivityType.TASK),
+                                Activity("or", None, ActivityType.OR, ["E", "F"]),
+                                Activity("E", None, ActivityType.TASK),
+                                Activity("F", None, ActivityType.END),
+                                Activity("end", None, ActivityType.END)]),
+            {"my_key": "a value"},
+            {"ctx_key": "ctx value"},
+            self.callback_getter,
+            NoNluAdapter([]),
+            lambda k: None)
+
+    def test_path_1(self):
+        path = [["", "gateway", []],
+                ["xor", "xor", []],
+                ["A", "A", []],
+                ["", "gateway", []],
+                ["par", "par", []],
+                ["C", "C", []],
+                ["", "par", []],
+                ["D", "D", ["par"]],
+                ["", "par", ["par"]],
+                [None, "gateway", []],
+                ["or", "or", ["gateway"]],
+                ["E", "E", ["gateway", "or"]],
+                ["", "or", ["gateway", "or"]],
+                [None, "gateway", ["gateway"]],
+                ["or", "or", ["gateway"]],
+                ["F", "F", ["gateway", "or"]],
+                ["", "F", ["gateway", "or"]]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+    def test_path_2(self):
+        path = [["", "gateway", []],
+                ["xor", "xor", []],
+                ["B", "B", []],
+                ["", "gateway", []],
+                ["or", "or", []],
+                ["E", "E", ["or"]],
+                ["", "or", ["or"]],
+                [None, "gateway", []],
+                ["par", "par", ["gateway"]],
+                ["D", "D", ["gateway"]],
+                ["", "par", ["gateway"]],
+                ["C", "C", ["gateway", "par"]],
+                ["", "par", ["gateway", "par"]],
+                ["D", "D", ["gateway", "par"]],
+                ["", "par", ["gateway", "par"]],
+                [None, "gateway", ["gateway"]],
+                [None, "end", []],
+                ["", "end", []]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+
+class TestFrameworkBehaviourXOR(TestCase):
+    @staticmethod
+    def callback_getter(_):
+        return lambda d, k, c: Response(k, c, True, choice=d["choice"] if "choice" in d else "")
+
+    def setUp(self) -> None:
+        self.my_framework = Framework(
+            Process(first_activity_id="start",
+                    activities=[Activity("start", "gateway", ActivityType.START),
+                                Activity("gateway", "end", ActivityType.XOR, ["xor", "par", "or"]),
+                                Activity("xor", None, ActivityType.XOR, ["A", "B"]),
+                                Activity("A", None, ActivityType.TASK),
+                                Activity("B", None, ActivityType.TASK),
+                                Activity("par", None, ActivityType.PARALLEL, ["C", "D"]),
+                                Activity("C", None, ActivityType.TASK),
+                                Activity("D", None, ActivityType.TASK),
+                                Activity("or", None, ActivityType.OR, ["E", "F"]),
+                                Activity("E", None, ActivityType.TASK),
+                                Activity("F", None, ActivityType.END),
+                                Activity("end", None, ActivityType.END)]),
+            {"my_key": "a value"},
+            {"ctx_key": "ctx value"},
+            self.callback_getter,
+            NoNluAdapter([]),
+            lambda k: None)
+
+    def test_path_1a(self):
+        path = [["", "gateway", []],
+                ["xor", "xor", []],
+                ["A", "A", []],
+                ["", "end", []],
+                ["", "end", []]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+    def test_path_1b(self):
+        path = [["", "gateway", []],
+                ["par", "par", []],
+                ["C", "C", []],
+                ["", "par", []],
+                ["D", "D", ["par"]],
+                ["", "par", ["par"]],
+                [None, "end", []],
+                ["", "end", []]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+    def test_path_1c(self):
+        path = [["", "gateway", []],
+                ["or", "or", []],
+                ["F", "F", ["or"]],
+                ["", "F", ["or"]]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+    def test_path_2a(self):
+        path = [["", "gateway", []],
+                ["par", "par", []],
+                ["D", "D", []],
+                ["", "par", []],
+                ["C", "C", ["par"]],
+                ["", "par", ["par"]],
+                ["D", "D", ["par"]],
+                ["", "par", ["par"]],
+                [None, "end", []],
+                ["", "end", []]]
+        for index, step in enumerate(path):
+            self.my_framework.handle_data_input({"choice": step[0]} if step[0] != "" else {"data": "value"})
+            self.assertEqual(self.my_framework._current.id, step[1], f"Step index when test failed: {index}, "
+                                                                     f"expected: {step[1]}")
+            self.assertEqual(self.my_framework._ctx[CTX_COMPLETED], step[2], f"Step index when test failed: {index}")
+
+
 class TestFramework(TestCase):
     @staticmethod
-    def callback_getter(name):
+    def callback_getter(_):
         return lambda d, k, c: Response(k, c, True)
 
     def setUp(self) -> None:
